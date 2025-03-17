2d0d4c8e2d671b9b54a0d4a1680578e8d0dcf93a
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-17 20:38:04 -0300

    Aula.cs

---
+++ Aula.cs
@@ -0,0 +1,36 @@
+_1°_
+requisito não funcinal (rnf)
+requisito funcional (rf)
+
+A) não funcional (rnf)
+B) não funcional (rnf)
+C) funcional (rf)
+D) funcional (rf)
+E) funcional (rf)
+
+_2°_
+
+RN01 - O quarto possui preço, descrição, não tem serviço do quarto e nem frigobar
+RN02 - As reservas são garantidas por meio de pagamento de diária
+RF01 - Verificar se existem quartos disponíveis
+RF02 - Solicitar os dados do cliente
+RF03 - Armazenar informações da reserva: datas entrada, saida, desconto e n° do quarto
+RF04 - Cancelar a reserva por falta de pagamento
+RF05 - Gerar relatório de reservas canceladas
+RF06 - Gerar relatório de reservas não pagas
+RF07 - Gerar relatório de reservas e serem efetivadas no dia
+RF08 - Gerar relatório de de reservas de um determinado período de tempo
+RF09 - 
+
+_3°_
+
+Sistema de Gerenciamento de Biblioteca
+
+RN01 - modernizar o sistema de gerenciamento em fichas catalográficas e processsos manuais.
+RF01 - Um novo sistema, chamado Bibliotech.
+RF02 - Sistema ser acessivel para funcionários e alunos.
+RF03 - Fazer uma "biblioteca" online, disponibilizando livros, periódicos, artigos, e outros materiais.
+RF04 - Criar relatório de empréstimo, devolução e reserva de materiais.
+RF05 - Criar um relatório sobre relatórios e estatísticas sobreo o uso da biblioteca.
+
+_4°_
