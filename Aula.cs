bed10321e09b478b7c007d2647048f434d48be8d
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-24 20:04:06 -0300

    Aula.cs

--- Aula.cs
+++ Aula.cs
@@ -12,43 +12,50 @@ _2°_ (No Projetor)
 
 RN01 - O quarto possui preço, descrição, não tem serviço do quarto e nem frigobar
 RN02 - As reservas são garantidas por meio de pagamento de diária
-RF01 - Verificar se existem quartos disponíveis
-RF02 - Solicitar os dados do cliente
-RF03 - Armazenar informações da reserva: datas entrada, saida, desconto e n° do quarto
-RF04 - Cancelar a reserva por falta de pagamento
-RF05 - Gerar relatório de reservas canceladas
-RF06 - Gerar relatório de reservas não pagas
-RF07 - Gerar relatório de reservas e serem efetivadas no dia
-RF08 - Gerar relatório de de reservas de um determinado período de tempo
+
+RF01 - Solicitar os dados do cliente
+RF02 - Armazenar informações da reserva: datas entrada, saida, desconto e n° do quarto
+RF03 - Cancelar a reserva por falta de pagamento
+RF04 - Gerar relatório de reservas canceladas
+RF05 - Gerar relatório de reservas não pagas
+RF06 - Gerar relatório de reservas e serem efetivadas no dia
+RF07 - Gerar relatório de de reservas de um determinado período de tempo
+
+RNF01 - Verificar se existem quartos disponíveis
 
 _3°_
 
 Desenvolvimento de um Aplicativo de Agendamento de Consultas Médicas
 
 RN01 - Criar um aplicativo para uma rede de farmácias
+
 RF01 - Criar uma comunicação entre as farmácias
 RF02 - O cliente poder comprar pela WEB
-RF03 - O cliente poder ver se há produto em estoque
 RF04 - Gerenciar compras de produtos
 RF05 - Disponibilizar uma farmácia conseguir consultar a outra para saber se há um produto em outra farmácia
-RF06 - Criar um gerenciamento de funcionários, e de clientes
-RF07 - Criar um cadastro de cliente
-RF08 - gerenciamento de compras e vendas
-RF09 - gerenciamento de pagamento
-RF10 - emissão de nota fiscal
-RF11 - emissão de relatórios contábeis e compras pela web
+RF06 - Criar um cadastro de cliente
+RF07 - emissão de relatórios contábeis e compras pela web
+
+RNF01 - O cliente poder ver se há produto em estoque
+RNF02 - Criar um gerenciamento de funcionários, e de clientes
+RNF03 - gerenciamento de compras e vendas
+RNF04 - gerenciamento de pagamento
+RNF05 - emissão de nota fiscal
+
 
 _4°_
 
 Sistema de Gerenciamento de Biblioteca
 
 RN01 - modernizar o sistema de gerenciamento em fichas catalográficas e processsos manuais.
-RNF01 - Ser online e acessível para funcionários e alunos.
+  
 RF01 - Um novo sistema, chamado Bibliotech.
 RF02 - Sistema ser acessivel para funcionários e alunos.
 RF03 - Fazer uma "biblioteca" online, disponibilizando livros, periódicos, artigos, e outros materiais.
 RF04 - Criar relatório de empréstimo, devolução e reserva de materiais.
 RF05 - Criar um relatório sobre relatórios e estatísticas sobreo o uso da biblioteca.
+  
+RNF01 - Ser online e acessível para funcionários e alunos.
 
 // Obs: RNF: criar um sistema online e acessível para funcionários e alunos.
 // O certo da questão é:
@@ -60,18 +67,20 @@ _5°_
 Desenvolvimento de um Aplicativo de Agendamento de Consultas Médicas
 
 RN01 - Consultas agendadas no mínimo com 24 horas de antecedência
+
 RF01 - Enviar lembretes de consulta aos pacientes por meio de notificações
-RF02 - Aplicativo disponível todos os dias e a qualquer hora
-RF03 - Aplicativo deve permitir o agendamento de acordo com a agenda dos médicos
-RF04 - Permitir agendar o dia e o horário e o médico
-RF05 - Poder cancelar consultas com antecedência
-RF06 - Permitir reagendar consultas para outros horários disponíveis
-RF07 - Permitir a visualização dos horários disponíveis de cada médico
-RF08 - Permitir a visualização dos currículos dos médicos
-RF09 - Permitir a visualização dos históricos de consulta dos mesmos
-RF10 - Ser compatível com dispositivos movéis
-RF11 - Permitir aplicativo no modo easy
-RF12 - Aplicativo deve ser bem otimizado
-RF13 - Deve conter uma alta segurança para os dados de cada paciente
-RF14 - O aplicativo deve disponibilizar cancelamento de consulta com no mínimo 12 horas de antecedência
-RF15 - Criar limite de 3 consultas futuras por vez
+RF02 - Aplicativo deve permitir o agendamento de acordo com a agenda dos médicos
+RF03 - Permitir agendar o dia e o horário e o médico
+RF04 - Permitir reagendar consultas para outros horários disponíveis
+RF05 - Permitir a visualização dos horários disponíveis de cada médico
+RF06 - Permitir a visualização dos currículos dos médicos
+RF07 - Permitir a visualização dos históricos de consulta dos mesmos
+RF08 - Permitir aplicativo no modo easy
+RF09 - Criar limite de 3 consultas futuras por vez
+
+RNF01 - Aplicativo disponível todos os dias e a qualquer hora
+RNF02 - Poder cancelar consultas com antecedência
+RNF03 - Aplicativo deve ser bem otimizado
+RNF04 - Deve conter uma alta segurança para os dados de cada paciente
+RNF05 - Ser compatível com dispositivos movéis
+RNF06 - O aplicativo deve disponibilizar cancelamento de consulta com no mínimo 12 horas de antecedência
